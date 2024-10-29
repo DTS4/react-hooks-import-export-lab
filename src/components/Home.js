88c67a1941889350f764650c325946838827deeb
Keith <keith.githinji@student.moringaschool.com>
2024-10-29 15:37:47 +0300

    Completed the lab

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,8 +4,12 @@ function Home() {
   return (
     <div id="home">
       <h1>
-        {username} is a Web Developer from {city}
+        {username} is a Web Developer from {city} 
       </h1>
     </div>
   );
 }
+
+export default Home;
+export const username='Liza';
+export const city='New York';
